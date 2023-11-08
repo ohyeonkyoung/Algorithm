9f0094d786ccc64bbf6acfcf246da8b5a885fb84
ykoh <127908327+ohyeonkyoung@users.noreply.github.com>
2023-11-08 20:41:21 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int A = scan.nextInt();
+		
+		if(A%4==0 && A%100!=0 || A%400==0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
+}
